0ea8758b2324fc307bb923b92e62c198a19dcafb
AHMEDNABIB <nabibpallab@gmail.com>
2024-06-12 19:14:44 +0600

    add validation

--- src/components/BasicFrom.tsx
+++ src/components/BasicFrom.tsx
@@ -10,8 +10,9 @@ type FormValues = {
 
 function BasicFrom() {
 	const from = useForm<FormValues>()
-	const { register , control , handleSubmit} = from
+	const { register , control , handleSubmit , formState} = from
 	
+	const { errors } = formState;
 	// const {name,ref,onChange,onBlur}= register("username")
 
 	const onSubmit = (data: FormValues) => {
@@ -24,13 +25,43 @@ function BasicFrom() {
 			<form onSubmit={handleSubmit(onSubmit)}>
 				<label htmlFor="username">Username</label>
 				{/* <input type="text" id="username" name={name} ref={ref} onChange={onChange} /> */}
-				<input type="text" id="username" {...register("username")} />
+				<input
+					type="text"
+					id="username"
+					{...register("username", {
+						required: {
+							value: true,
+							message: "Username is required",
+						},
+					})}
+				/>
+				<p>{errors.username?.message }</p>
 
 				<label htmlFor="email">E-mail</label>
-				<input type="email" id="email" {...register("email")} />
+				<input
+					type="email"
+					id="email"
+					{...register("email", {
+						pattern: {
+							value: /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+							message: "Invalid email format",
+						},
+					})}
+				/>
+				<p>{ errors.email?.message}</p>
 
 				<label htmlFor="channel">Channel</label>
-				<input type="text" id="channel" {...register("channel")} />
+				<input
+					type="text"
+					id="channel"
+					{...register("channel", {
+						required: {
+							value: true,
+							message: "Channel is required",
+						},
+					})}
+				/>
+				<p>{ errors.channel?.message}</p>
 
 				<button>Submit</button>
 			</form>
